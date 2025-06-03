a0b97509aae0bb22f5ee6cfa31d0f0df2a511d86
Dearie <eburujacintha@gmail.com>
2025-06-03 15:54:20 +0400

    exported all controllers

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,13 @@
+const authController = require('./authController');
+const userController = require('./userController');
+const bookController = require('./bookController');
+const scholarController = require('./scholarController');
+const downloadController = require('./downloadController');
+
+module.exports = {
+  authController,
+  userController,
+  bookController,
+  scholarController,
+  downloadController
+};
